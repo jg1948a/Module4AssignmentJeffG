d015ab733e1becf94639b633420b589950785e8f
jg1948a <78171714+jg1948a@users.noreply.github.com>
2021-02-26 09:32:09 -0500

    Add files via upload

---
+++ Lecture50/js/script.js
@@ -0,0 +1,47 @@
+// Short hand array creation
+var fruits = ["Peanuts", "Lime", "Coconuts", "Jam"]; 
+	console.log(fruits);
+	for (var i = 0; i < fruits.length; i++) {
+  		var fruitsName = fruits[i];
+   		 if (fruitsName.substr(0,1).toLowerCase() === 'j') {
+        console.log("Goodbye " + fruitsName);
+    	} else { console.log("Hello " + fruitsName);
+    }
+  } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
